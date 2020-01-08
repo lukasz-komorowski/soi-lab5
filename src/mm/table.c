21de8c05a77c30d2965bbd4095fb486f870aa074
Lukasz Komorowski <komorosiu96@gmail.com>
2020-01-08 18:34:49 +0100

    Corrected code formatting

--- src/mm/table.c
+++ src/mm/table.c
@@ -11,21 +11,21 @@
 #include "param.h"
 
 /* Miscellaneous */
-char core_name[] = "core";	/* file name where core images are produced */
+char core_name[] = "core"; /* file name where core images are produced */
 
-_PROTOTYPE (int (*call_vec[NCALLS]), (void) ) = {
+_PROTOTYPE(int(*call_vec[NCALLS]), (void)) = {
 	no_sys,		/*  0 = unused	*/
-	do_mm_exit,	/*  1 = exit	*/
+	do_mm_exit, /*  1 = exit	*/
 	do_fork,	/*  2 = fork	*/
 	no_sys,		/*  3 = read	*/
 	no_sys,		/*  4 = write	*/
 	no_sys,		/*  5 = open	*/
 	no_sys,		/*  6 = close	*/
-	do_waitpid,	/*  7 = wait	*/
+	do_waitpid, /*  7 = wait	*/
 	no_sys,		/*  8 = creat	*/
 	no_sys,		/*  9 = link	*/
 	no_sys,		/* 10 = unlink	*/
-	do_waitpid,	/* 11 = waitpid	*/
+	do_waitpid, /* 11 = waitpid	*/
 	no_sys,		/* 12 = chdir	*/
 	no_sys,		/* 13 = time	*/
 	no_sys,		/* 14 = mknod	*/
@@ -34,16 +34,16 @@ _PROTOTYPE (int (*call_vec[NCALLS]), (void) ) = {
 	do_brk,		/* 17 = break	*/
 	no_sys,		/* 18 = stat	*/
 	no_sys,		/* 19 = lseek	*/
-	do_getset,	/* 20 = getpid	*/
+	do_getset,  /* 20 = getpid	*/
 	no_sys,		/* 21 = mount	*/
 	no_sys,		/* 22 = umount	*/
-	do_getset,	/* 23 = setuid	*/
-	do_getset,	/* 24 = getuid	*/
+	do_getset,  /* 23 = setuid	*/
+	do_getset,  /* 24 = getuid	*/
 	no_sys,		/* 25 = stime	*/
-	do_trace,	/* 26 = ptrace	*/
-	do_alarm,	/* 27 = alarm	*/
+	do_trace,   /* 26 = ptrace	*/
+	do_alarm,   /* 27 = alarm	*/
 	no_sys,		/* 28 = fstat	*/
-	do_pause,	/* 29 = pause	*/
+	do_pause,   /* 29 = pause	*/
 	no_sys,		/* 30 = utime	*/
 	no_sys,		/* 31 = (stty)	*/
 	no_sys,		/* 32 = (gtty)	*/
@@ -60,8 +60,8 @@ _PROTOTYPE (int (*call_vec[NCALLS]), (void) ) = {
 	no_sys,		/* 43 = times	*/
 	no_sys,		/* 44 = (prof)	*/
 	no_sys,		/* 45 = unused	*/
-	do_getset,	/* 46 = setgid	*/
-	do_getset,	/* 47 = getgid	*/
+	do_getset,  /* 46 = setgid	*/
+	do_getset,  /* 47 = getgid	*/
 	no_sys,		/* 48 = (signal)*/
 	no_sys,		/* 49 = unused	*/
 	no_sys,		/* 50 = unused	*/
@@ -76,27 +76,27 @@ _PROTOTYPE (int (*call_vec[NCALLS]), (void) ) = {
 	do_exec,	/* 59 = execve	*/
 	no_sys,		/* 60 = umask	*/
 	no_sys,		/* 61 = chroot	*/
-	do_getset,	/* 62 = setsid	*/
-	do_getset,	/* 63 = getpgrp	*/
+	do_getset,  /* 62 = setsid	*/
+	do_getset,  /* 63 = getpgrp	*/
 
-	do_ksig,	/* 64 = KSIG: signals originating in the kernel	*/
-	no_sys,		/* 65 = UNPAUSE	*/
-	no_sys, 	/* 66 = unused  */
-	no_sys,		/* 67 = REVIVE	*/
-	no_sys,		/* 68 = TASK_REPLY  */
-	no_sys,		/* 69 = unused	*/
-	no_sys,		/* 70 = unused	*/
-	do_sigaction,	/* 71 = sigaction   */
-	do_sigsuspend,	/* 72 = sigsuspend  */
-	do_sigpending,	/* 73 = sigpending  */
-	do_sigprocmask,	/* 74 = sigprocmask */
-	do_sigreturn,	/* 75 = sigreturn   */
-	do_reboot,	/* 76 = reboot	*/
-	do_svrctl,	/* 77 = svrctl	*/
+	do_ksig,		/* 64 = KSIG: signals originating in the kernel	*/
+	no_sys,			/* 65 = UNPAUSE	*/
+	no_sys,			/* 66 = unused  */
+	no_sys,			/* 67 = REVIVE	*/
+	no_sys,			/* 68 = TASK_REPLY  */
+	no_sys,			/* 69 = unused	*/
+	no_sys,			/* 70 = unused	*/
+	do_sigaction,   /* 71 = sigaction   */
+	do_sigsuspend,  /* 72 = sigsuspend  */
+	do_sigpending,  /* 73 = sigpending  */
+	do_sigprocmask, /* 74 = sigprocmask */
+	do_sigreturn,   /* 75 = sigreturn   */
+	do_reboot,		/* 76 = reboot	*/
+	do_svrctl,		/* 77 = svrctl	*/
 
 	/* lab5 */
-	do_worst_fit,	/* 78 = worst_fit */
-	do_hole_map,	/* 79 = hole_map */
+	do_worst_fit, /* 78 = worst_fit */
+	do_hole_map,  /* 79 = hole_map */
 };
 /* This should not fail with "array size is negative": */
 extern int dummy[sizeof(call_vec) == NCALLS * sizeof(call_vec[0]) ? 1 : -1];
